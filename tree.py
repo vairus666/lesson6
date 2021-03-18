ec2feee075c03eb75b3e086719a8b33b0d56b91b
vairus666 <43839255+vairus666@users.noreply.github.com>
2021-03-18 03:21:16 +0300

    Create tree.py

---
+++ tree.py
@@ -0,0 +1,53 @@
+class Node:
+
+    def __init__(self, data):
+
+        self.left = None
+        self.right = None
+        self.data = data
+# Insert Node
+    def insert(self, data):
+
+        if self.data:
+            if data < self.data:
+                if self.left is None:
+                    self.left = Node(data)
+                else:
+                    self.left.insert(data)
+            elif data > self.data:
+                if self.right is None:
+                    self.right = Node(data)
+                else:
+                    self.right.insert(data)
+        else:
+            self.data = data
+
+# Print the Tree
+    def PrintTree(self):
+        if self.left:
+            self.left.PrintTree()
+        print( self.data),
+        if self.right:
+            self.right.PrintTree()
+
+# Inorder traversal
+# Left -> Root -> Right
+    def inorderTraversal(self, root):
+        res = []
+        if root:
+            res = self.inorderTraversal(root.left)
+            res.append(root.data)
+            res = res + self.inorderTraversal(root.right)
+        return res
+
+tree = Node (2)
+tree.insert(3)
+tree.insert(4)
+tree.insert(0)
+tree.insert(8)
+tree.insert(5)
+tree.insert(1)
+tree.insert(2)
+tree.insert(6)
+tree.insert(7)
+print(tree.inorderTraversal(tree))
